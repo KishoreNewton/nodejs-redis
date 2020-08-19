6bae6fda7e2ef928e3785ca4161ee31240c47f4d
Kishore Newton <kishorenke@gmail.com>
2020-08-19 16:57:52 +0530

    added the app module

--- app.js
+++ app.js
@@ -13,3 +13,9 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+})
+
+module.exports = app
